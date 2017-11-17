097e215b6dbbaa8c3a6d72e128a10b321a44841a
Mark Woodward <markwood117@gmail.com>
2017-11-17 13:00:35 -0800

    test all planetary conversion methods to be working as intended;

--- js/ageConversion.js
+++ js/ageConversion.js
@@ -4,26 +4,26 @@ export class AgeConversion {
     this.age=age;
   }
 
-  yearToSeconds(age) {
-    // return (age*(365*24*60*60));
+  yearToSeconds() {
+    return (this.age*(365*24*60*60));
   };
 
-  mercuryAge(age) {
-    // return age*.24;
+  mercuryAge() {
+    return parseFloat(this.age*.24);
   };
-  venusAge(age) {
-    // return age*.62;
+  venusAge() {
+    return parseFloat(this.age*.62);
   };
-  marsAge(age) {
-    // return age*1.88;
+  marsAge() {
+    return parseFloat(this.age*1.88);
   };
-  jupiterAge(age) {
-    // return age*11.86;
+  jupiterAge() {
+    return parseFloat(this.age*11.86);
   };
 
-  getLifeExpectancy(age) {
+  getLifeExpectancy() {
     // let currentYr = new Date.getFullYear();
-    // if((currentYr-age)<2000){return 70;}
+    // if((currentYr-this.age)<2000){return 70;}
     // else {return 75;}
   };
 }
--- spec/ageConversion-spec.js
+++ spec/ageConversion-spec.js
@@ -5,4 +5,24 @@ describe("AgeConversion",function(){
     let testModule = new AgeConversion(1);
     expect(testModule.yearToSeconds()).toEqual(31536000);
   });
+
+  it("test earth year to mercury year; should return .24 for input of 1",function(){
+    let testModule = new AgeConversion(1);
+    expect(testModule.mercuryAge()).toEqual(.24);
+  });
+
+  it("test earth year to venus year; should return .62 for input of 1",function(){
+    let testModule = new AgeConversion(1);
+    expect(testModule.venusAge()).toEqual(.62);
+  });
+
+  it("test earth year to mars year; should return 1.88 for input of 1",function(){
+    let testModule = new AgeConversion(1);
+    expect(testModule.marsAge()).toEqual(1.88);
+  });
+
+  it("test earth year to jupiter year; should return 11.86 for input of 1",function(){
+    let testModule = new AgeConversion(1);
+    expect(testModule.jupiterAge()).toEqual(11.86);
+  });
 });
