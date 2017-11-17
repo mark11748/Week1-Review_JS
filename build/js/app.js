4d694250b7089f421cd1b70345e5a5438575e72b
Mark Woodward <markwood117@gmail.com>
2017-11-17 15:53:48 -0800

    add min viable product;

--- build/js/app.js
+++ build/js/app.js
@@ -67,11 +67,40 @@ $(document).ready(function () {
     $("#output-list li").remove();
     var age = $("#age").val();
     var ageConverter = new _ageConversion.AgeConversion(age);
-    $("#output-list").append("<li>" + "You are: " + ageConverter.yearToSeconds(age) + " seconds olds" + "</li>");
-    $("#output-list").append("<li>" + "You are: " + ageConverter.mercuryAge(age) + " year olds on mercury" + "</li>");
-    $("#output-list").append("<li>" + "You are: " + ageConverter.venusAge(age) + " year olds on venus" + "</li>");
-    $("#output-list").append("<li>" + "You are: " + ageConverter.marsAge(age) + " year olds on mars" + "</li>");
-    $("#output-list").append("<li>" + "You are: " + ageConverter.jupiterAge(age) + " year olds on jupiter" + "</li>");
+    var expectancy = ageConverter.getLifeExpectancy();
+
+    $("#output-list").append("<li id=\"earth\">" + "<p>You are: " + ageConverter.yearToSeconds(age) + " seconds old.</p>" + "<p>Your life expectancy is: " + expectancy + " years old.</p>" + "</li>");
+    if (age >= expectancy) {
+      $("#output-list #earth").append("<p>Congradulations you're not dead!</p>");
+    }
+    if (age < expectancy) {
+      $("#output-list #earth").append("<p>You have approximately: " + (expectancy - age) + " years left.</p>");
+    }
+    $("#output-list #earth").append("<br>");
+
+    $("#output-list").append("<li id=\"mercury\">" + "<p>You are: " + ageConverter.mercuryAge(age) + " years olds on mercury</p>" + "<p>Your life expectancy on mercury is: " + ageConverter.mercuryAge(expectancy) + " years old.</p>" + "</li>");
+    if (age < expectancy) {
+      $("#output-list #mercury").append("<p>You have approximately: " + ageConverter.mercuryAge(expectancy - age) + " years left.</p>");
+    }
+    $("#output-list #mercury").append("<br>");
+
+    $("#output-list").append("<li id=\"venus\">" + "<p>You are: " + ageConverter.venusAge(age) + " years olds on venus</p>" + "<p>Your life expectancy on venus is: " + ageConverter.venusAge(expectancy) + " years old.</p>" + "</li>");
+    if (age < expectancy) {
+      $("#output-list #venus").append("<p>You have approximately: " + ageConverter.venusAge(expectancy - age) + " years left.</p>");
+    }
+    $("#output-list #venus").append("<br>");
+
+    $("#output-list").append("<li id=\"mars\">" + "<p>You are: " + ageConverter.marsAge(age) + " years olds on mars</p>" + "<p>Your life expectancy on mars is: " + ageConverter.marsAge(expectancy) + " years old.</p>" + "</li>");
+    if (age < expectancy) {
+      $("#output-list #mars").append("<p>You have approximately: " + ageConverter.marsAge(expectancy - age) + " years left.</p>");
+    }
+    $("#output-list #mars").append("<br>");
+
+    $("#output-list").append("<li id=\"jupiter\">" + "<p>You are: " + ageConverter.jupiterAge(age) + " years olds on jupiter</p>" + "<p>" + "Your life expectancy on jupiter is: " + ageConverter.jupiterAge(expectancy) + "years old.</p>" + "</li>");
+    if (age < expectancy) {
+      $("#output-list #jupiter").append("<p>You have approximately: " + ageConverter.jupiterAge(expectancy - age) + " years left.</p>");
+    }
+    $("#output-list #jupiter").append("<br>");
   });
 });
 
